e6e87135ec71e5935419f9a127abea9142f5f44e
Maciej Marciniak <maciej.r.marciniak@student.put.poznan.pl>
2017-05-10 20:28:25 +0200

    skrypt serwo i elektrozamek

--- Raspberry/Skrypt inteligentny zamek serwomechanizm.py
+++ Raspberry/Skrypt inteligentny zamek serwomechanizm.py
@@ -1,16 +1,26 @@
+#!/usr/bin/python
 import time
 import RPi.GPIO as GPIO
+import cv2
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
-p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
-p.start(0)
-while 1:
-    for dc in range(0, 101, 5):
-        p.ChangeDutyCycle(dc)
-        time.sleep(0.1)
-    for dc in range(100, -1, -5):
-        p.ChangeDutyCycle(dc)
-        time.sleep(0.1)
+try:
+    p = GPIO.PWM(12, 50)  # pin=12 czestotliwosc=50Hz prawo=3 lewo=11
+    GPIO.output(16,1)
+    p.start(3)
+    while True:
+        raw_input("Nacisnij Enter aby otworzyc")
+        p.ChangeDutyCycle(11)
+        GPIO.output(16,0)
+        print "Otwarty"
+        time.sleep(10)
+        p.ChangeDutyCycle(3)
+        GPIO.output(16,1)
+        print "Zamkniety"
+except Error(Exception):
+    pass
 p.stop()
 GPIO.cleanup()
