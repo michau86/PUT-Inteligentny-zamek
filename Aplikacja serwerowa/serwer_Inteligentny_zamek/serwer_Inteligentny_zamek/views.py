42ba1fecb78ffd02a92931dea5a1a2fd1c08ef39
damifil <damifil15@gmail.com>
2017-05-29 10:22:37 +0200

    dodanie api do wylogowania
    dodane (nie przetestowane) api do wylogowania

--- Aplikacja serwerowa/serwer_Inteligentny_zamek/serwer_Inteligentny_zamek/views.py
+++ Aplikacja serwerowa/serwer_Inteligentny_zamek/serwer_Inteligentny_zamek/views.py
@@ -18,6 +18,9 @@ userpassword="1234"
 databasename="inteligentny_zamek_db"
 databaseaddres="localhost"
 
+
+
+# api do logowania
 @csrf_exempt
 def api_login(request):
     username = request.POST.get('username')
@@ -38,9 +41,8 @@ def api_login(request):
 
     cursor.execute("SELECT PASSWORD FROM users WHERE login='%s'" %username)
     data = cursor.fetchone()[0]
-    print data
-    print password
-    if(data==password):
+
+    if(data==password and data!=""):
 
         cursor.execute("UPDATE users SET TOKEN = '%s' WHERE LOGIN = '%s'" % (token, username))
         data = cursor.fetchone()
@@ -51,7 +53,7 @@ def api_login(request):
 
         return JsonResponse({"status": "ERROR PASSWORD", "token": "invalid"})
 
-
+#api do rejestrowania
 @csrf_exempt
 def api_register(request):
     if request.method == 'POST':
@@ -61,13 +63,10 @@ def api_register(request):
         surname=request.POST.get('surname')
         # Open database connection
         db = MySQLdb.connect(databaseaddres, username, userpassword, databasename)
-
         # prepare a cursor object using cursor() method
         cursor = db.cursor()
-
         # execute SQL query using execute() method.
         cursor.execute("SELECT Login FROM users WHERE login='%s'" % login)
-
         # Fetch a single row using fetchone() method.
         data = cursor.fetchone()
         if(data != None):
@@ -84,3 +83,26 @@ def api_register(request):
             except:
                 db.rollback()
                 return JsonResponse({"status": "ERROR"})
+
+
+#api do wylogowania
+@csrf_exempt
+def api_logout(request):
+    if request.method == 'POST':
+        login =request.POST.get('login')
+        token=request.POST.get('token')
+
+        db = MySQLdb.connect(databaseaddres, username, userpassword, databasename)
+        cursor = db.cursor()
+        cursor.execute("SELECT TOKEN FROM users WHERE login='%s'" % login)
+        data = cursor.fetchone()[0]
+
+        #jezeli token jest poprawny to nastepuje wylogowanie
+        if(data==token):
+            #aktualizacja tokena na pusty
+            cursor.execute("UPDATE users SET TOKEN = '%s' WHERE LOGIN = '%s'" % ("", username))
+            data = cursor.fetchone()
+
+            return JsonResponse({"status": "logout"})
+        else:
+            return JsonResponse({"status": "invalid token"})
