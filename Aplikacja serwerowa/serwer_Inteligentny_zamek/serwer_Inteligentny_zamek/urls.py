7f58efc2a81a3d71f9fce2559ebe9fc23af4cf9f
Maciej Marciniak <maciej.r.marciniak@student.put.poznan.pl>
2017-07-03 01:37:54 +0200

    do poprzedniego commita

--- Aplikacja serwerowa/serwer_Inteligentny_zamek/serwer_Inteligentny_zamek/urls.py
+++ Aplikacja serwerowa/serwer_Inteligentny_zamek/serwer_Inteligentny_zamek/urls.py
@@ -14,6 +14,7 @@ urlpatterns = {
     url(r'^api/admin/generate_new_certificate/$', views.api_admin_generate_new_certificate, name='api_admin_generate_new_certificate'),
     url(r'^api/admin/download/all_certificate/$', views.api_admin_download_all_certificate, name='api_admin_download_all_certificate'),
     url(r'^api/admin/deactivation/$', views.api_admin_deactivation, name='api_admin_deactivation'),
+    url(r'^api/admin/register_waiting/$', views.api_admin_register_waiting, name='api_admin_register_waiting'),
     # url(r'^api/admin/register_decision/$', views.api_admin_register_decision, name='api_admin_register_decision'),  # po zatwierdzeniu uzytkownika generowany jest klucz prywatny i przesyaany do uzytkownika
     url(r'^api/admin/cetificate_waiting/$', views.api_admin_cetificate_waiting, name='api_admin_cerificate_waiting'),
     # url(r'^api/admin/certificate_decision/$', views.api_admin_cetificate_decision, name='api_admin_certificate_decision'),
