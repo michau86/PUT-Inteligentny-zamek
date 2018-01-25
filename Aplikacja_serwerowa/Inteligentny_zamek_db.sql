3d75a2af2363fcfb75a3fe4909f8cda9ecde0727
Maciej <maciej.r.marciniak@student.put.poznan.pl>
2018-01-25 21:55:23 +0100

    Commit po rzeźni

--- Aplikacja_serwerowa/Inteligentny_zamek_db.sql
+++ Aplikacja_serwerowa/Inteligentny_zamek_db.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Sty 2018, 16:06
--- Wersja serwera: 10.1.21-MariaDB
--- Wersja PHP: 5.6.30
+-- Czas generowania: 25 Sty 2018, 21:52
+-- Wersja serwera: 10.1.25-MariaDB
+-- Wersja PHP: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -185,7 +187,27 @@ INSERT INTO `access_to_locks` (`ID`, `ID_KEY`, `DATE`, `ACCESS`) VALUES
 (145, 5, '2017-12-11 08:40:14', 1),
 (146, 5, '2017-12-11 08:40:24', 1),
 (147, 5, '2017-12-11 08:40:34', 1),
-(148, 5, '2017-12-11 08:48:25', 1);
+(148, 5, '2017-12-11 08:48:25', 1),
+(149, 14, '2018-01-25 05:30:11', 0),
+(150, 14, '2018-01-25 09:34:40', 0),
+(151, 14, '2018-01-25 09:35:49', 0),
+(152, 14, '2018-01-25 09:36:15', 0),
+(153, 14, '2018-01-25 09:36:52', 0),
+(154, 14, '2018-01-25 09:42:57', 0),
+(155, 14, '2018-01-25 09:44:10', 0),
+(156, 14, '2018-01-25 13:53:35', 0),
+(157, 14, '2018-01-25 14:17:41', 1),
+(158, 14, '2018-01-25 14:18:09', 1),
+(159, 14, '2018-01-25 14:18:42', 1),
+(160, 14, '2018-01-25 14:21:32', 1),
+(161, 14, '2018-01-25 14:22:10', 1),
+(162, 14, '2018-01-25 14:25:31', 1),
+(163, 14, '2018-01-25 14:25:57', 1),
+(164, 14, '2018-01-25 14:26:28', 1),
+(165, 14, '2018-01-25 14:28:50', 0),
+(166, 14, '2018-01-25 14:29:11', 0),
+(167, 14, '2018-01-25 14:29:30', 0),
+(168, 14, '2018-01-25 14:29:49', 1);
 
 -- --------------------------------------------------------
 
@@ -275,7 +297,7 @@ CREATE TABLE `auth_user` (
 --
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
-(1, 'pbkdf2_sha256$36000$6cFw4EZmhCsa$eKSZxMZJy0kSoJY2Zks3FCiI/CVOaKYO+3tN1GQXBWA=', '2018-01-18 13:49:49', 1, 'damian', '', '', 'a@wp.pl', 1, 1, '2018-01-15 19:57:44'),
+(1, 'pbkdf2_sha256$36000$6cFw4EZmhCsa$eKSZxMZJy0kSoJY2Zks3FCiI/CVOaKYO+3tN1GQXBWA=', '2018-01-25 15:30:37', 1, 'damian', '', '', 'a@wp.pl', 1, 1, '2018-01-15 19:57:44'),
 (2, 'pbkdf2_sha256$20000$5Ig7Px8IhL3g$KusfaBd3JpFhZcC1Nq4IxbDPMqijpfeVu2NylDi1sQ0=', NULL, 1, 'dam', '', '', 'a@wp.pl', 1, 1, '2018-01-15 20:32:18'),
 (3, 'pbkdf2_sha256$36000$g9CoxGW6ol8G$05BUSYWEPLxqTv9LXWf7PIbSan+h/RWNvlAARXIRzqs=', '2018-01-16 00:41:23', 1, 'Damian2', '', '', 'a@wp.pl', 1, 1, '2018-01-15 23:20:43');
 
@@ -400,18 +422,6 @@ INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALU
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `django_site`
---
-
-CREATE TABLE `django_site` (
-  `id` int(11) NOT NULL,
-  `domain` varchar(100) NOT NULL,
-  `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Struktura tabeli dla tabeli `locks`
 --
 
@@ -420,16 +430,16 @@ CREATE TABLE `locks` (
   `NAME` varchar(30) NOT NULL,
   `LOCALIZATION` text CHARACTER SET utf8 COLLATE utf8_polish_ci,
   `MAC_ADDRESS` varchar(17) NOT NULL,
-  `ADMIN_KEY` text NOT NULL
+  `People_inside` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Zrzut danych tabeli `locks`
 --
 
-INSERT INTO `locks` (`ID_LOCK`, `NAME`, `LOCALIZATION`, `MAC_ADDRESS`, `ADMIN_KEY`) VALUES
-(1, 'Zamek testowy', 'Opis zamka', 'B8:27:EB:FC:73:A1', 'B8:27:EB:FC:73:A2'),
-(2, 'Drugi zamek', 'Drugi zamek testowy', 'B8:27:EB:FC:73:A2', 'B8:27:EB:FC:73:A2');
+INSERT INTO `locks` (`ID_LOCK`, `NAME`, `LOCALIZATION`, `MAC_ADDRESS`, `People_inside`) VALUES
+(1, 'Zamek testowy', 'Opis zamka', 'B8:27:EB:FC:73:A1', 0),
+(2, 'Drugi zamek', 'Drugi zamek testowy', 'B8:27:EB:FC:73:A2', 0);
 
 -- --------------------------------------------------------
 
@@ -445,13 +455,13 @@ CREATE TABLE `locks_keys` (
   `FROM_DATE` datetime NOT NULL,
   `TO_DATE` datetime NOT NULL,
   `ISACTUAL` datetime DEFAULT NULL,
-  `MONDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
-  `TUESDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
-  `WEDNESDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
-  `THURSDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
-  `FRIDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
-  `SATURDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
-  `SUNDAY` varchar(10) COLLATE utf8_polish_ci DEFAULT NULL,
+  `MONDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `TUESDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `WEDNESDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `THURSDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `FRIDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `SATURDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
+  `SUNDAY` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
   `IS_PERNAMENT` tinyint(1) NOT NULL,
   `NAME` varchar(30) COLLATE utf8_polish_ci NOT NULL,
   `SURNAME` varchar(50) COLLATE utf8_polish_ci NOT NULL
@@ -471,9 +481,7 @@ INSERT INTO `locks_keys` (`ID_KEY`, `ID_LOCK`, `ID_USER`, `LOCK_KEY`, `FROM_DATE
 (9, 2, 137, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIzZiAeNgUim3WtgWJTYpECspbZ2uHETGXQi3CIyR1lMqw3rLI3inCb9c6csHSe7lGlAsaLaMblB8/pCZfOrG0ltuBgozuQu1eP9tosKi2WCTBv+WrOSGdF0ase0LfzoMMASszKHtLLHgGVYmbCr4AlUEaWk53UvABMg5o1TI3kwIDAQAB', '2017-12-29 00:00:00', '2017-12-30 00:00:00', NULL, '2-4;', '5-6;7-8;', '', '', '', '', '', 0, 'dgsd', 'dfg'),
 (10, 2, 145, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJFkLpvqtuxWZb5hQye9JHYwo6Z5IN3ebmhvpOvzjyh6+htvQIg1jCCfT/bgH9bOuwdKfTNnNHcSExsbbCdtAnFsPRkT49NVbJb6NJ2Iz3wBa33lFSsm5YstY0hyTdmBsz03LpgH6aRcXEroVWmC3EgeNgTpGA2nokxu+Iq3QcWwIDAQAB', '2018-01-01 00:00:00', '2018-01-31 00:00:00', '2018-01-06 19:16:15', '6:07-12:07', '', '', '', '', '', '', 0, 'zx', 'xczv'),
 (11, 2, 137, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJC419YwnThjrVCDMeqSygHPf9/SBbQgLo8rx3yzsHuGBPNqKoJsUsJR3YbonQQUrNw0bTWinJoEAn5c9dcospa5zsMRBeA8KxeQYvn07L4IQvaX0mxvFpdQU5VXXXuGf3+OmKZhP5Afj616Laj+4gLKk/o7WVxVu17BDXFldNmQIDAQAB', '2018-01-01 00:00:00', '2018-01-16 00:00:00', NULL, '10:07-12:0', '', '', '', '', '', '', 0, 'asd', 'sdfa'),
-(12, 1, 142, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDFMVfjxUht4m6gOnOQ8HsxkZyeM/5RKh/zXmrDtLyaAENLDYspx9rY/VEvdirO7NckljdX6y99YoJSL8QttKMCupZrXS+k7BHT/tLjeIY+hqFt6w5Hg1x/n5RCQMufYmzoUSYTxxCNlmAboPZdRBlbtgUQMjiTAbgYJsdeSJRVOQIDAQAB', '2018-01-01 00:00:00', '2018-01-17 00:00:00', NULL, '21:27-23:5', '', '', '', '', '', '', 0, 'sdfg', 'dsfg'),
-(13, 2, 142, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCiCS6xk61L9JQAmnYqJRQhbpf+wd87I+dXrKgyNv35/yNgHHHY4yDOaHt3gZWHPNjIz0dpt/f2TjK96J03uwMMOWQjCSAAGe23FznljBbpmanX34lRgQCzAxUX6BzWOE5ghuhY9unyAgI14VFbUqJlohVLz2L3rwLytv89XJs+/QIDAQAB', '2018-01-17 00:00:00', '2018-01-17 00:00:00', NULL, '10:27-12:2', '', '', '', '', '', '', 0, 'sdfg', 'dsfg'),
-(14, 2, 1, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhfu664DccYOI1SlvNV6emQTG2wMMDjXBZyCv041fYGaigwo9x7xaMUtmKhu5WFKxVrz8lrr2h9OHcJFsHb6jP+a79SCjBjEdvagTPRQsXKhafq/5OkWdTwSqmfLOdshummAncxChdxIUGfaVlt68gn0U8DCAfztbsBDt3JqXCAQIDAQAB', '2018-01-06 00:00:00', '2018-01-16 00:00:00', NULL, '10:02-13:0', '', '', '', '', '', '', 0, '', '');
+(14, 2, 148, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhfu664DccYOI1SlvNV6emQTG2wMMDjXBZyCv041fYGaigwo9x7xaMUtmKhu5WFKxVrz8lrr2h9OHcJFsHb6jP+a79SCjBjEdvagTPRQsXKhafq/5OkWdTwSqmfLOdshummAncxChdxIUGfaVlt68gn0U8DCAfztbsBDt3JqXCAQIDAQAB', '2018-01-01 00:00:00', '2018-01-31 00:00:00', NULL, '10:02-13:00', '', '', '00:01-23:59', '', '', '', 1, '', '');
 
 -- --------------------------------------------------------
 
@@ -505,13 +513,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`ID_USER`, `LOGIN`, `PASSWORD`, `NAME`, `SURNAME`, `TOKEN`, `ISACTIVATED`, `IS_ADMIN`, `PUBLIC_KEY`, `Serial_number`, `Validitiy_period`, `Version`, `Signature_Algorithm_Identifier`, `Hash_Algorithm`) VALUES
 (1, 'potestujmy', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'saddf', 'fsdasdf', NULL, 2, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDH9LZSsOFVnkGeJd4lzwYxDDOXQlsJGWit2Spp\nwiHRG+cSv6BSbrTwVuMFErkyhpCzLtXJBG42N7/v0bUmk4NcfETheG1d7y6/4XKCQ7HiZZh226E5\n6/0azb9gPMFLq6MRCmDnyFAq3qXPMzjP+nCG7ayBpyD++f4n9OYe0VXBPwIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC5IlbTGE42Iz7dniaCfP1fJOLrhnoz+mCiURSlcVnGqOzb1vXJADYrsoTgLC6JTOt91WxhRpZQYMYCdvaR6ApwzH+uwQI1jUityYrxarDmBNK1UU6CwFS/qTytm5u5MJWc5t2FPmS1/t9LeADd9EiFioIe9Nt9AGZaSw2gr5ln6QIDAQAB', '2018-01-07 12:11:08', 1, 'RSA', 'SHA-256'),
 (134, 'Maciej1', '01D48A697442ED3231773C944973E87315FFD44232A2503D1B68B02F1D677FF5', 'Maciej', 'Marciniak', 'aaa', 2, 1, 'ddd', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCyoCd559OOPfZAtzHqj31zcBqLglDekGGaGzJ8AQpVZWoWSs8GSm14r0LdO7qb0QYjExp0diGiyhSkgAhJ+jDJFyD2v4M62OebiCgl35y2a0EZeYaVWKFyNTax3IDKtW/ZvZP7H25ahP1Ea6BlpFFYNxEi52cJG7LP06JtnMBDMwIDAQAB', '2018-12-10 23:50:32', 1, 'RSA', 'SHA-256'),
-(136, 'Maciej3', '01D48A697442ED3231773C944973E87315FFD44232A2503D1B68B02F1D677FF5', 'Mm', 'Mm', NULL, 1, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHZcJstVK0Vg5ZEnHq0UTC1dhM8h8srCR3jTl3\nSpk+V0xWapLga3gRSN3ne0nPjRw+YpRe+ZAWd5E9OuEUBi0UwfYYEmLU3t3djMR4J2q++tyvQ4g8\ny/Q2mOLnXzw5gDtl3jfhdmZOVIEJeJkpqCVINNRNcHSAg+P7KGAiUFm7JQIDAQAB\n', '', NULL, 1, 'RSA', 'SHA-256'),
-(137, 'Damian', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'D am ian', 'Xff', NULL, 0, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9cVoaNxtzARE4uFW29KZPOUI0yHt8fZzESNht\n/UGQoiOizX+3mholAbRXFEVxg7DXs0qa6UD6bPE9hLqm5+3H8LhSWuY+Hbyg6mPFFn6o66gW3aY+\nXCV5fzlsnj7crK3ohuYedhJ+psST2FsIJImWPbJy1ytm1JteyuimqRJbMQIDAQAB\n', NULL, NULL, 1, 'RSA', 'SHA-256'),
+(137, 'Damian', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'D am ian', 'Xff', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1r6tsAtcwxJAQysUMCB0HCME/wfbj5vgs/FrLE/tcRNskXyI/KXqMjFv033PF1bZGqGc4+1mL31HdT5gU9RmeGjgNzca5g2pMNuri70foCoJnTotkKeD5dh6UHM4I/V4ezQlW5g4s+X9Z7+f/djIZJaAwL8+8AxdyJxkIfRf7AwIDAQAB', 0, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9cVoaNxtzARE4uFW29KZPOUI0yHt8fZzESNht\n/UGQoiOizX+3mholAbRXFEVxg7DXs0qa6UD6bPE9hLqm5+3H8LhSWuY+Hbyg6mPFFn6o66gW3aY+\nXCV5fzlsnj7crK3ohuYedhJ+psST2FsIJImWPbJy1ytm1JteyuimqRJbMQIDAQAB\n', NULL, NULL, 1, 'RSA', 'SHA-256'),
 (140, 'potestujmy4', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'saddf', 'fsdasdf', NULL, 0, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/tweAAg0+3Ipt/QT9/VUOdek3+vaqQXCzcCPO\nuw0yC/v2CWh3eHDH1mtWr+3w/bp2HW/1DJjj0DJfxFviF0qCpBYF+EcPNj+4BZvu7yQO1twQueh5\n2E0BIjkZ2zS1vNFx/IRYQGb+W8Av8hCVgDtHwSAnAOy4zXQrmaftP9jm3QIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTirrwd839ZBF/vys/gK9jfoRzQP9E9l8bKyeIoVDt421TJRSmdB6H8x7SzU8p6XqcYh6rVukn6YjE4Fu2tv1dsAhjnHXwwHfGyi0jJCks/8FfL6OVWLlloW3f8EqSAjc2cvpavuGpuJfL8U1ROcuBmmHaKq+4w230zqNgJt9duwIDAQAB', '2018-12-15 20:02:57', 1, 'RSA', 'SHA-256'),
-(142, 'potestujmy6', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'Maciej', 'MM', NULL, 1, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD9XjXDsVNKkPkjr+ht4+dfnY+JxTMtgtZqBUzK\nvVzyRd/DOdtnI+aqawiqCBMJ2BRvUjvYccEGsdOqlLvzP4N8XQfpeSE8C03BiELtlE01z6hfxzXB\nAjaA7Q7BY0D5quudaZFERxd8kHrONEi5vffzClhhn17wYfdgVNDPHx+m5wIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCWwtC+8su68l2m7ETV4T2lKtq23SxbGkktziirA+BGN68eslip5QEWlKi+8LoBqL0r10svZ6/6LIzNLDmuIGDLwNfI4rwOtZzsBHteEQTPrkzQHgDacdK44fT/omWopor5cjkm+RVByOE/npAZWiIiVgxZU2x38bpRVIUQGvLbhQIDAQAB', '2018-12-15 20:07:38', 1, 'RSA', 'SHA-256'),
 (145, 'DamianTest10', '851EB1F79381A0A9771954693A6ACF988DD5D1FFC23C266A64071C661978BE93', 'Damian', 'Test', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDXomqGBabdtDY052WVQF5esCcntn6lcqvxlUs4wnGfhvKM6B1yX+xMCuaiHaTkIGibb2fVnD9Tonh6xnBKCux1YdoCzivcR8kDUE2udq4zdOyFE1r8aHPEiwErbCf1om70MipJR2AX+8Ek83O99SPwfw+qrDLxcMYURVZyiIY7DQIDAQAB', 0, 1, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6bIhggJ76s5na/Qfi8lj0l8mHz2k+m6fQ8KoD\nOVxSVgpa0cbSsR2NGtbQkcEAqTk7S8PSCmRhbpmCMmU6IzLhV1fiSjs4kCEUH0FcObREk+Qrlqtn\nOfWHKRuINoOOIOazZD80/RhVOYWp1ryM8LKaJNX+Fz1A2pDE6UygFs5c1QIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCyl5+tiTKv2+Y+UY2XR0rBdlL9Z+QtAfOibhdeP2do0X5uLnaNhYhmSvaxr7q8uNNXrO3EV4UUCzJoM3vZGKCYHbidIFGo1z108gF9+7OEhpzUFJVzFLOzjxwEwXwBbN8ymIja8fZevlSpIh06nce9Qt3+XJfD3j7ETA+p6JFNrQIDAQAB', '2019-01-07 19:44:40', 1, 'RSA', 'SHA-256'),
-(146, 'oczekuje1', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'Imie1', 'Nazwisko1', NULL, 1, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCsxxyIkcCBKpyI+41iGsUZBSOg3lJzVqNumbFu\nVbsss6nthVSc+mCW92+AMpI+RCa47telYm7mGtIq+RU6eT2ERog/vp/1XtsuajKioV7P3jb6A4q5\nQhq+cct3Q2wqyurXCO2Gj3sszUqOD+Pw4q+OjLsk9jQqxxkLWix1u3If1QIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCQR2EG+kl+5wwZrW6kXJjIExgLazO2rv5fDf7znwhuw6CKgB+eg6kd4TL4+/aqjj1UdzxwJck11flNStW1ftEdbkKnCxvVXnk79YjJAPwoaxq158ShAelqx6FICLCn9QgZaIeYrMc19OQmX97r+2ntMvSbvF5nRem3vYzZbCGOzwIDAQAB', '2019-01-08 00:12:27', 1, 'RSA', 'SHA-256'),
-(147, 'oczekuje2', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'Imie2', 'Nazwisko2', NULL, 1, 0, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCeMT7l7wYeamQFxYw85S9NWqRUFbk/KU+2xSPS\n1fUEQytklg1DWwP65Vpqi8ITPEXloR318GLghHbQyY/pK3/RKtmmMrYZD7c3RoKYDaMhuvz5Sgmf\ne4aoammFENOtbAz2jRwK8wPQE0gJ66nuroUA0PRGYzGaRMaOVUU9mHqbdwIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCT7KEKyt09L3EzYScewk0ZtoLjMOJfV8ra363MDfpNCjWwQ9ogFw4Tqx3/EC2f6TAW1DJQBSNCHBX0uziUA/bmN5fWKkYxjF+tL6kQjskABt/mIMFAuEiUHAYZRY17J7Endiox4vlkJpVT709V4inDAA41oZyx+kokcNC6SG82dwIDAQAB', '2019-01-08 00:12:40', 1, 'RSA', 'SHA-256');
+(148, 'TestowyZwykly', '913BEEB024BAA2BE29C3D19B1F859B5508BDFCE177554446073478C4B86F6A9F', 'Testowy', 'Testowy', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQChBReWAEB6IGasvVaS4KuQEAIQxHkJAWe24Qat8BODEgTJ+/wx0ck3S/5dLcUIlo+FF8hqtPmK2jzHfOTglbzijnH212O4F3YoxqnY8xAn6vXzzMNjJ2ZvzYPQbU366yYdM7bOYtlX/8rbOa0n3zyN9oYDtOXQfTqKjorqVztjVwIDAQAB', 0, 1, 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDW0Ix06jGKlkPPtm6barlzUdXbPoJolcYJJkkD\n2Poy4AfkqDrObNwWFO4xEVmyNNAyw0MX9J6XWcDuZub46odWJrxlZd74MUcVvzhBBEBaLczzouqa\ns1OQV/HgyvmEgaWKbjSWHW6gAH0HdRdkNJUIbYoFNdwZ7BzpoQfzCIIBqwIDAQAB\n', 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCljA9k/ZLOziZumhjIw1v0csb4U8MQD6TYKyfEHW5TlmTZ0GW7vlB+eX9z7yHprHjH+B/GNMwiQOPLNpK6sihQ+GpI1vfxMEsoj6jlq25NdmQ5vTwKYSRbc+NJX0WUFzzQ46LNbntio75vRyE5tyrfHLREfPw+4MDzYPqti1hZnQIDAQAB', '2019-01-25 14:29:38', 1, 'RSA', 'SHA-256');
 
 -- --------------------------------------------------------
 
@@ -524,14 +529,6 @@ CREATE TABLE `wait_locks_keys` (
   `ID_LOCK` int(10) NOT NULL,
   `ID_USER` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
---
--- Zrzut danych tabeli `wait_locks_keys`
---
-
-INSERT INTO `wait_locks_keys` (`ID_KEY`, `ID_LOCK`, `ID_USER`) VALUES
-(11, 1, 145),
-(12, 1, 145);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -618,13 +615,6 @@ ALTER TABLE `django_session`
   ADD KEY `django_session_expire_date_a5c62663` (`expire_date`);
 
 --
--- Indexes for table `django_site`
---
-ALTER TABLE `django_site`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `django_site_domain_a2e37b91_uniq` (`domain`);
-
---
 -- Indexes for table `locks`
 --
 ALTER TABLE `locks`
@@ -664,7 +654,7 @@ ALTER TABLE `wait_locks_keys`
 -- AUTO_INCREMENT dla tabeli `access_to_locks`
 --
 ALTER TABLE `access_to_locks`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=169;
 --
 -- AUTO_INCREMENT dla tabeli `auth_group`
 --
@@ -691,11 +681,6 @@ ALTER TABLE `auth_user`
 ALTER TABLE `django_content_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT dla tabeli `django_site`
---
-ALTER TABLE `django_site`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT dla tabeli `locks`
 --
 ALTER TABLE `locks`
@@ -709,7 +694,7 @@ ALTER TABLE `locks_keys`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID_USER` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
+  MODIFY `ID_USER` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
 --
 -- AUTO_INCREMENT dla tabeli `wait_locks_keys`
 --
@@ -724,6 +709,14 @@ ALTER TABLE `wait_locks_keys`
 --
 ALTER TABLE `auth_permission`
   ADD CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+
+--
+-- Ograniczenia dla tabeli `locks_keys`
+--
+ALTER TABLE `locks_keys`
+  ADD CONSTRAINT `locks_keys_ibfk_1` FOREIGN KEY (`ID_LOCK`) REFERENCES `locks` (`ID_LOCK`),
+  ADD CONSTRAINT `locks_keys_ibfk_2` FOREIGN KEY (`ID_USER`) REFERENCES `users` (`ID_USER`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
